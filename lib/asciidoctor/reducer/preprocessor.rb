6853508729a99fa47c77c9af0be485f26fcc78d6
Dan Allen <dan.j.allen@gmail.com>
2022-01-28 03:00:32 -0700

    rename PreprocessorReader ext module to PreprocessorDirectiveTracker

--- lib/asciidoctor/reducer/preprocessor.rb
+++ lib/asciidoctor/reducer/preprocessor.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'ext/asciidoctor/preprocessor_reader'
+require_relative 'preprocessor_directive_tracker'
 
 module Asciidoctor::Reducer
   class Preprocessor < ::Asciidoctor::Extensions::Preprocessor
     def process doc, reader
       return if doc.options[:reduced]
-      reader.singleton_class.prepend AsciidoctorExt::PreprocessorReader
+      reader.singleton_class.prepend PreprocessorDirectiveTracker
       reader.instance_variable_set :@x_include_replacements, ([{ drop: [] }].extend Current)
       reader.instance_variable_set :@x_parents, [0]
       nil
