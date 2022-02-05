40af0cd416057059a324f4202cdc865077fa4ac3
Dan Allen <dan.j.allen@gmail.com>
2022-02-05 02:12:50 -0700

    simplify how include replacement target is tracked

--- lib/asciidoctor/reducer/preprocessor_directive_tracker.rb
+++ lib/asciidoctor/reducer/preprocessor_directive_tracker.rb
@@ -6,8 +6,9 @@ module Asciidoctor::Reducer
     attr_reader :x_include_replacements
 
     def self.extended instance
-      instance.instance_variable_set :@x_include_replacements, ([{ drop: [] }].extend CurrentPointer)
-      instance.instance_variable_set :@x_parents, [0]
+      instance.instance_variable_set :@x_include_replacements, ([{ drop: [] }].extend CurrentPosition)
+      instance.instance_variable_set :@x_include_directive_line, nil
+      instance.instance_variable_set :@x_include_pushed, nil
     end
 
     def preprocess_conditional_directive keyword, target, delimiter, text
@@ -34,69 +35,58 @@ module Asciidoctor::Reducer
 
     def preprocess_include_directive target, attrlist
       @x_include_directive_line = %(include::#{target}[#{attrlist}])
-      @x_push_include_called = false
+      @x_include_pushed = false
       inc_lineno = @lineno - 1 # we're currently on the include line, which is 1-based
       result = super
-      return result if @x_push_include_called
-      parent_depth = (parents = @x_parents).length
-      # depth change is guaranteed to be >= 0 under normal conditions
-      unless @include_stack.length < parent_depth
-        parents << @x_include_replacements.length.pred
-        parent_depth += 1
+      if @x_include_pushed
+        @x_include_directive_line = @x_include_pushed = nil
+        return result
       end
       inc_lines = ((line = lines[0].to_s).start_with? 'Unresolved directive in ') && (line.end_with? ']') ? [line] : []
-      push_include_replacement inc_lines, parent_depth, inc_lineno
+      push_include_replacement inc_lineno, inc_lines
+      @x_include_directive_line = @x_include_pushed = nil
       result
     end
 
     def push_include data, file, path, lineno, attrs
-      @x_push_include_called = true
+      @x_include_pushed = true
       inc_lineno = @lineno - 2 # we're below the include line, which is 1-based
       prev_inc_depth = @include_stack.length
       result = super
-      parent_depth = (parents = @x_parents).length
-      # push_include did not push to the stack
-      if (inc_depth = @include_stack.length) == prev_inc_depth
-        depth_change = inc_depth - (parent_depth - 1)
-      else
-        depth_change = inc_depth - parent_depth
-        inc_lines = lines
-      end
-      if depth_change > 0
-        parents << @x_include_replacements.length.pred
-        parent_depth += 1
-      elsif depth_change < 0
-        parent_depth -= (parents.slice! parent_depth + depth_change, -depth_change).length
-      end
-      push_include_replacement inc_lines, parent_depth, inc_lineno
+      inc_lines = lines if @include_stack.length > prev_inc_depth
+      push_include_replacement inc_lineno, inc_lines
       result
     end
 
     def pop_include
-      @x_include_replacements.current = @x_include_replacements[@x_include_replacements.current[:into] || 0]
+      @x_include_replacements.pos = @x_include_replacements.current[:into] unless @x_include_pushed
       super
     end
 
     private
 
-    def push_include_replacement inc_lines, parent_depth, inc_lineno
+    def push_include_replacement inc_lineno, inc_lines
       @x_include_replacements << {
-        into: @x_parents[parent_depth - 1],
+        into: @x_include_replacements.pos,
         lineno: inc_lineno,
         line: @x_include_directive_line,
         lines: inc_lines || [],
         drop: [],
       }
-      @x_include_replacements.current = @x_include_replacements[-1] if inc_lines
+      @x_include_replacements.pos = @x_include_replacements.length - 1 if inc_lines
       nil
     end
   end
 
-  module CurrentPointer
-    attr_accessor :current
+  module CurrentPosition
+    attr_accessor :pos
 
     def self.extended instance
-      instance.current = instance[-1]
+      instance.pos = instance.length - 1
+    end
+
+    def current
+      self[@pos]
     end
   end
 end
