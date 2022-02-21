12045496745996a252ba3f8fb37813e9bd884542
Dan Allen <dan.j.allen@gmail.com>
2022-02-21 04:01:45 -0700

    resolves #36 only mix in preprocessor conditional tracker if :preserve_conditionals is not set

---
+++ lib/asciidoctor/reducer/conditional_directive_tracker.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Asciidoctor::Reducer
+  module ConditionalDirectiveTracker
+    def preprocess_conditional_directive keyword, target, delimiter, text
+      skip_active = @skipping
+      depth = @conditional_stack.size
+      cond_lineno = @lineno
+      result = super
+      return result if @skipping && skip_active
+      drop = @x_include_replacements.current[:drop] ||= []
+      if (depth_change = @conditional_stack.size - depth) < 0
+        if skip_active
+          drop.push(*(drop.pop..cond_lineno))
+        else
+          drop << cond_lineno
+        end
+      elsif depth_change > 0 || cond_lineno == @lineno
+        drop << cond_lineno
+      else
+        drop << [cond_lineno, text]
+      end
+      result
+    end
+  end
+end
--- lib/asciidoctor/reducer/preprocessor.rb
+++ lib/asciidoctor/reducer/preprocessor.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-require_relative 'preprocessor_directive_tracker'
+require_relative 'include_directive_tracker'
+require_relative 'conditional_directive_tracker'
 
 module Asciidoctor::Reducer
   class Preprocessor < ::Asciidoctor::Extensions::Preprocessor
-    def process _, reader
-      reader.extend PreprocessorDirectiveTracker
+    def process doc, reader
+      reader.extend IncludeDirectiveTracker
+      reader.extend ConditionalDirectiveTracker unless doc.options[:preserve_conditionals]
     end
   end
 end
