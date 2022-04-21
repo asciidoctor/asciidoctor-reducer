4a510502132cf2ae270a462940f0f656f5a4fecb
Dan Allen <dan.j.allen@gmail.com>
2022-04-21 00:17:19 -0600

    consolidate nested conditional

--- lib/asciidoctor/reducer/include_directive_tracker.rb
+++ lib/asciidoctor/reducer/include_directive_tracker.rb
@@ -16,10 +16,9 @@ module Asciidoctor::Reducer
       inc_lineno = @lineno # we're currently on the include line, which is 1-based
       result = super
       unless @x_reducer[:include_pushed]
-        if ((ln = peek_line true)&.end_with? ']') && !(unresolved = ln.start_with? 'Unresolved directive in ')
-          if inc_lineno == @lineno && (unresolved = ln.start_with? 'link:')
-            ln = %(#{ln.slice 0, (ln.length - 1)}role=include])
-          end
+        if ((ln = peek_line true)&.end_with? ']') && !(unresolved = ln.start_with? 'Unresolved directive in ') &&
+            inc_lineno == @lineno && (unresolved = ln.start_with? 'link:')
+          ln = %(#{ln.slice 0, (ln.length - 1)}role=include])
         end
         push_include_replacement inc_lineno, (unresolved ? [ln] : []), unresolved
       end
